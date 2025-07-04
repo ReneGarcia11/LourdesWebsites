d10c4648fbfa177e081f4518b2e00bf25c9c86ac
Rene Eduardo <a22310546@ceti.mx>
2025-07-04 13:12:09 -0600

    Other

---
+++ src/app/opiniones/page.jsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+import OpinionForm from '../../components/OpinionForm' // o donde esté tu componente
+
+export default function OpinionesPage() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>Deja tu opinión</h1>
+      <OpinionForm />
+    </main>
+  )
+}
