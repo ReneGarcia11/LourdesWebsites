2ae06661ceb18901822f24ebe2aa083b2e23d592
Rene Eduardo <a22310546@ceti.mx>
2025-07-04 13:59:45 -0600

    last

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/api/opinions/:path*',
-        headers: [
-          { 
-            key: 'Access-Control-Allow-Origin', 
-            value: 'https://opinions.crisisyduelo.com' 
-          },
-          { 
-            key: 'Access-Control-Allow-Methods', 
-            value: 'GET,POST,OPTIONS' 
-          },
-          { 
-            key: 'Access-Control-Allow-Headers', 
-            value: 'Content-Type' 
-          }
-        ]
-      }
-    ];
-  }
-};
+const nextConfig = {};
 
-// Exportación ES Modules (MJS)
 export default nextConfig;
