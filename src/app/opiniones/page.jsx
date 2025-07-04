34b136b2e47d05bd24e0f3d3e1fcd975f75bd911
Rene Eduardo <a22310546@ceti.mx>
2025-07-04 13:15:10 -0600

    missing "s"

--- src/app/opiniones/page.jsx
+++ src/app/opiniones/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import OpinionForm from '../../components/OpinionForm' // o donde esté tu componente
+import OpinionForm from '../../components/OpinionsForm' // o donde esté tu componente
 
 export default function OpinionesPage() {
   return (
