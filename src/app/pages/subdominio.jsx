2871335da4f1a0b75c31fbf8d5226672a58b696b
Rene Eduardo <a22310546@ceti.mx>
2025-07-04 12:40:11 -0600

    new rute

---
+++ src/app/pages/subdominio.jsx
@@ -0,0 +1,12 @@
+// /src/pages/subdominio.jsx
+import React from 'react';
+import OpinionsForm from '../subdominio/OpinionsForm';
+
+export default function SubdominioPage() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Opiniones</h1>
+      <OpinionsForm />
+    </div>
+  );
+}
