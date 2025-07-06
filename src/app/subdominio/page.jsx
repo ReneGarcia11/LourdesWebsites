3856bff4b958a797f71b2e8c73406ffdffb324a5
Rene Eduardo <a22310546@ceti.mx>
2025-07-06 03:28:36 -0600

    sub

---
+++ src/app/subdominio/page.jsx
@@ -0,0 +1,9 @@
+// src/app/subdominio/page.tsx
+export default function OpinionesForm() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Deja tu opinión</h1>
+      {/* Aquí va el formulario para capturar opiniones */}
+    </div>
+  );
+}
